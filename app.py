e1a5141a34e5258574b3326b74d6ba3d632f7816
A2Pro <87340157+A2Pro@users.noreply.github.com>
2024-09-11 11:54:50 -0700

    Final1

--- app.py
+++ app.py
@@ -13,10 +13,38 @@ def process_cases(file_path):
 
     return cases
 
+def process_urls(file_path):
+    with open(file_path, "r") as f:
+        urls = f.readlines()
+    return urls
+
+def process_dates(file_path):
+    with open(file_path, "r") as f:
+        dates = f.readlines()
+    return dates
+
+def process_keywords(file_path):
+    with open(file_path, "r") as f:
+        currentkeywords = f.readlines()
+    finalkeywords = []
+    for keyword in currentkeywords:
+        if "[]![]" not in keyword:
+            finalkeywords.append(keyword)
+    return finalkeywords
+
+
 @app.route('/')
 def index():
     cases = process_cases('txtfiles/completed.txt')
-    return render_template('index.html', cases=cases)
+    urls = process_urls("txtfiles/completedresponses.txt")
+    dates = process_dates("txtfiles/date.txt")
+    keywords = process_keywords("txtfiles/keywords.txt")
+    
+    # Zip the data before passing it to the template
+    combined_data = zip(cases, urls, dates, keywords)
+    
+    return render_template('index.html', combined_data=combined_data)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- scraper.py
+++ scraper.py
@@ -64,4 +64,3 @@ def scrape():
 
     return
 
-scrape()
